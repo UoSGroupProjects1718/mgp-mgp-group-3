4b5c7664bd855f5181c415dc2f07e2da6d3d1d6f
Elliot Chester <elliotchester97@gmail.com>
2018-02-11 02:37:42 +0000

    Added first stages of the prototpe

---
+++ Prototype/Unity Project/L4-5 Group 3 Game/Assets/RecipeCardScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeCardScript : MonoBehaviour {
+
+    public GameObject recipeImage;
+    public GameObject ingredientList;
+    public GameObject textPrefab;
+
+    [System.Serializable]
+    public class Recipe
+    {
+        public Sprite image;
+        public string[] ingredients;
+    }
+
+    public Recipe[] recipes;
+
+    // Use this for initialization
+    void Start () {
+        GetNewRecipe();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GetNewRecipe()
+    {
+        int randomInt = Mathf.RoundToInt(Random.Range(0, recipes.Length ));
+
+        recipeImage.GetComponent<Image>().sprite = recipes[randomInt].image;
+
+        foreach (var ingredient in recipes[randomInt].ingredients)
+        {
+            GameObject newIngredient = Instantiate(textPrefab, ingredientList.transform);
+            newIngredient.GetComponent<Text>().text = ingredient;
+        }
+    }
+}

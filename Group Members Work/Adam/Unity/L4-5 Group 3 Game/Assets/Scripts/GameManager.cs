59660e47d5c9cad4e1da4a1cbf6b08d4de872065
Aespeers <S191345@uos.ac.uk>
2018-02-28 16:41:28 +0000

    Add start menu to prototype
    Hope this works for you ok

---
+++ Group Members Work/Adam/Unity/L4-5 Group 3 Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    static GameManager _instance;
+
+    public RecipeCardScript RCS;
+
+    public GameObject ingredientPrefab;
+
+    public GameObject player1Side;
+    public GameObject player1ButtonArea;
+    public GameObject player2Side;
+    public GameObject player2ButtonArea;
+
+    public float player1SideOpen;
+    public float player2SideOpen;
+    public float player1SideClosed;
+    public float player2SideClosed;
+
+    public int RecipeTypeID;
+
+    int lastPlayer = 2;
+
+    public RecipeCardScript.Ingredient[] neededIngredients;
+    public RecipeCardScript.Ingredient[] neededMixing;
+
+    public enum Turns{
+        none,
+        Player1,
+        Player2
+    }
+
+    public Turns currentTurn = Turns.none;
+
+    int[] neededAmts = new int[6];
+    int[] player1GivenAmt = new int[6];
+    int[] player2GivenAmt = new int[6];
+
+    int[] neededMixingAmts = new int[6];
+    int[] player1GivenMixingAmt = new int[6];
+    int[] player2GivenMixingAmt = new int[6];
+
+    static public GameManager Instance()
+    {
+        return _instance;
+    }
+
+    List<GameObject> currentButtons = new List<GameObject>();
+
+	void Start () {
+        if (_instance == null)
+        {
+            _instance = this.GetComponent<GameManager>();
+        }
+    }
+
+	void Update () {
+        if (currentTurn == Turns.Player1)
+        {
+            player1Side.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Mathf.MoveTowards(player1Side.GetComponent<RectTransform>().anchoredPosition.y,player1SideOpen, 2));
+            player2Side.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Mathf.MoveTowards(player2Side.GetComponent<RectTransform>().anchoredPosition.y, player2SideClosed, 2));
+        }
+        else
+        if (currentTurn == Turns.Player2)
+        {
+            player1Side.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Mathf.MoveTowards(player1Side.GetComponent<RectTransform>().anchoredPosition.y, player1SideClosed, 2));
+            player2Side.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Mathf.MoveTowards(player2Side.GetComponent<RectTransform>().anchoredPosition.y, player2SideOpen, 2));
+        }
+        else if(currentTurn == Turns.none)
+        {
+            player1Side.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Mathf.MoveTowards(player1Side.GetComponent<RectTransform>().anchoredPosition.y, player1SideClosed, 2));
+            player2Side.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, Mathf.MoveTowards(player2Side.GetComponent<RectTransform>().anchoredPosition.y, player2SideClosed, 2));
+        }
+    }
+
+    public void StartRound()
+    {
+        RCS.GetRecipe(RecipeTypeID);
+        currentTurn = Turns.none;
+        RCS.currentTarget = RecipeCardScript.Targets.middle;
+        RCS.StartCountdown();
+    }
+
+    public void StartMixingRound()
+    {
+        RCS.GetComponent<RectTransform>().anchoredPosition = RCS.rightPosition;
+        RCS.GetMixing();
+        lastPlayer = 2;
+        currentTurn = Turns.none;
+        RCS.currentTarget = RecipeCardScript.Targets.middle;
+        RCS.StartCountdown();
+    }
+
+    public void StartNextPlayer()
+    {
+        if (lastPlayer == 2)
+        {
+            SwitchTurn(1);
+            lastPlayer = 1;
+        }
+        else
+        {
+            SwitchTurn(2);
+            lastPlayer = 2;
+        }
+    }
+
+    public void SwitchTurn(int player)
+    {
+        if (player == 1)
+        {
+            currentTurn = Turns.Player1;
+        }
+        if (player == 2)
+        {
+            currentTurn = Turns.Player2;
+        }
+    }
+
+    public void UpdateNeededIngredients(RecipeCardScript.Ingredient[] ingredients)
+    {
+        DestroyAllButtons();
+
+        neededIngredients = ingredients;
+        int id = 0;
+        foreach (RecipeCardScript.Ingredient item in neededIngredients)
+        {
+            GameObject newButton1 = Instantiate(ingredientPrefab, player1ButtonArea.transform);
+            newButton1.name = item.name;
+            newButton1.GetComponent<Ingredient>().SetSprite(item.name);
+            newButton1.GetComponent<Ingredient>().SetID(id);
+            currentButtons.Add(newButton1);
+
+            GameObject newButton2 = Instantiate(ingredientPrefab, player2ButtonArea.transform);
+            newButton2.name = item.name;
+            newButton2.GetComponent<Ingredient>().SetSprite(item.name);
+            newButton2.GetComponent<Ingredient>().SetID(id);
+            currentButtons.Add(newButton2);
+            id++;
+            neededAmts[id] = item.amtNeeded;
+        }
+    }
+
+    public void UpdateNeededMixing(RecipeCardScript.Ingredient[] mixing)
+    {
+        DestroyAllButtons();
+
+        neededMixing = mixing;
+        int id = 0;
+        foreach (RecipeCardScript.Ingredient item in neededMixing)
+        {
+            GameObject newButton1 = Instantiate(ingredientPrefab, player1ButtonArea.transform);
+            newButton1.name = item.name;
+            newButton1.GetComponent<Ingredient>().SetSprite(item.name);
+            newButton1.GetComponent<Ingredient>().SetID(id);
+            currentButtons.Add(newButton1);
+
+            GameObject newButton2 = Instantiate(ingredientPrefab, player2ButtonArea.transform);
+            newButton2.name = item.name;
+            newButton2.GetComponent<Ingredient>().SetSprite(item.name);
+            newButton2.GetComponent<Ingredient>().SetID(id);
+            currentButtons.Add(newButton2);
+            id++;
+            neededAmts[id] = item.amtNeeded;
+        }
+    }
+
+    public void DestroyAllButtons()
+    {
+        if (currentButtons == null) { return; }
+
+        foreach (GameObject item in currentButtons)
+        {
+            Destroy(item);
+        }
+    }
+
+    public void SetGivenAmt(int id, int amt)
+    {
+        if(currentTurn == Turns.Player1)
+        {
+            player1GivenAmt[id] = amt;
+        }
+        else
+        {
+            player2GivenAmt[id] = amt;
+        }
+    }
+
+    public void Player1Submit()
+    {
+
+    }
+
+    public void Player2Submit()
+    {
+
+    }
+}
---
+++ Group Members Work/Adam/Unity/L4-5 Group 3 Game/Assets/Scripts/RecipeCardScript.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeCardScript : MonoBehaviour {
+
+    GameManager GM = GameManager.Instance();
+
+    [Header("Animation")]
+    public Vector3 rightPosition;
+    public Vector3 middlePosition;
+    public Vector3 leftPosition;
+    Vector3 targetPosition;
+
+    public float peekTime;
+
+    public enum Targets
+    {
+        right,
+        middle,
+        left
+    };
+
+    public Targets currentTarget = Targets.right;
+    RectTransform RT;
+
+    [Header("Recipe Card Objects")]
+    public GameObject recipeName;
+    public GameObject recipeImage;
+    public GameObject ingredientList;
+    public GameObject textPrefab;
+
+    int lastRecipeType = 0;
+    int lastRecipeID = 0;
+
+    bool countdown = false;
+    float countdownTimer = 3;
+
+    [System.Serializable]
+    public class Recipe
+    {
+        public string name;
+        public Sprite image;
+        public Ingredient[] ingredients;
+        public Ingredient[] mixing;
+    }
+
+    [System.Serializable]
+    public class Ingredient
+    {
+        public string name;
+        public int amtNeeded;
+    }
+
+    [System.Serializable]
+    public class Recipes
+    {
+        public string name;
+        public Recipe[] recipes;
+    }
+
+    [Header("Recipes")]
+    public Recipes[] RecipeTypes;
+
+    // Use this for initialization
+    void Start () {
+        //GetRecipe(0);
+        RT = this.GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetInput();
+        UpdateTarget();
+        Move();
+
+        if(countdown == true)
+        {
+            if (countdownTimer > 0)
+            {
+                countdownTimer -= Time.deltaTime;
+            }
+            else
+            {
+                countdown = false;
+                currentTarget = Targets.left;
+                GameManager.Instance().StartNextPlayer();
+            }
+        }
+    }
+
+    void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ClearRecipe();
+            GetRecipe(0);
+            Debug.Log("getting here");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ClearRecipe();
+            GetRecipe(0);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ClearRecipe();
+            GetRecipe(0);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ClearRecipe();
+            GetRecipe(0);
+        }
+
+        //debug Recipe Card Movement
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentTarget = Targets.left;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentTarget = Targets.middle;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentTarget = Targets.right;
+        }
+    }
+
+    void UpdateTarget()
+    {
+        switch (currentTarget)
+        {
+            case Targets.right:
+                targetPosition = rightPosition;
+                break;
+
+            case Targets.middle:
+                targetPosition = middlePosition;
+                break;
+
+            case Targets.left:
+                targetPosition = leftPosition;
+                break;
+        }
+    }
+
+    void Move()
+    {
+        if(RT.localPosition != targetPosition)
+        {
+            Vector2 newPos = Vector2.MoveTowards(RT.localPosition, targetPosition, 10f);
+            RT.localPosition = newPos;
+        }
+    }
+
+    void ClearRecipe()
+    {
+        foreach (Transform item in ingredientList.transform)
+        {
+            if (item.name != this.gameObject.name)
+            {
+                Destroy(item.gameObject);
+            }
+        }
+    }
+
+    public void GetRecipe(int RecipeType)
+    {
+        ClearRecipe();
+        int randomInt = Mathf.RoundToInt(Random.Range(0, RecipeTypes[RecipeType].recipes.Length ));
+
+        Recipe recipe = RecipeTypes[RecipeType].recipes[randomInt];
+
+        recipeName.GetComponent<Text>().text = recipe.name;
+        recipeImage.GetComponent<Image>().sprite = recipe.image;
+
+        foreach (var ingredient in recipe.ingredients)
+        {
+            GameObject newIngredient = Instantiate(textPrefab, ingredientList.transform);
+            newIngredient.GetComponent<Text>().text = ingredient.amtNeeded + "x " + ingredient.name;
+        }
+        GameManager.Instance().UpdateNeededIngredients(recipe.ingredients);
+        lastRecipeID = randomInt;
+    }
+
+    public void GetMixing()
+    {
+        ClearRecipe();
+        Recipe recipe = RecipeTypes[lastRecipeType].recipes[lastRecipeID];
+
+        foreach (var mix in recipe.mixing)
+        {
+            GameObject newIngredient = Instantiate(textPrefab, ingredientList.transform);
+            newIngredient.GetComponent<Text>().text = mix.amtNeeded + "x " + mix.name;
+        }
+        GameManager.Instance().UpdateNeededMixing(recipe.mixing);
+    }
+
+    public void StartCountdown()
+    {
+        countdownTimer = peekTime;
+        countdown = true;
+    }
+}
